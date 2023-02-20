4d983edb4f213b2afb19f23dccd5a26e9173d9b9
Matheus Henrique <69221000+matheushenriquemoreno@users.noreply.github.com>
2023-02-20 14:27:49 -0300

    adicionando Middleware global de erro

---
+++ CleanArchMVC/CleanArchMVC.WebUI/Helpers/ErrorHandlerMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Policy;
+
+namespace CleanArchMVC.WebUI.Helpers
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                var controllerDoErro = String.Join("-",context.Request.RouteValues.Values);
+ 
+                Console.WriteLine("Chamando Middleware global" + controllerDoErro);
+                
+            }
+        }
+    }
+}
